b1d6ba0beffc74a79e2a20eed2386da690cddd9f
serapkececi <serapkececi@aof.anadolu.edu.tr>
2023-12-21 22:14:44 +0300

    welcome screen created

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet,Text, View} from 'react-native';
 
-const Home = () => {
+export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to HomeScreen</Text>
+      <Text> Congratulations!!! </Text>
+      <Text>You have successfully logged in</Text>
     </View>
   );
-};
+}
 
-export default Home;
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  title:{
+    fontSize:16,
+    fontWeight:"bold",
+    marginBottom:10
+  },
+})
